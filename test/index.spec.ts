ae2140f1f6dad8a67b4e33985f81bcbcf8912075
Chouzz <zhouhua852@gmail.com>
2024-12-03 10:31:16 +0800

    Update index.spec.ts

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,9 @@
-import { myPackage } from '../src';
-
 describe('index', () => {
   describe('myPackage', () => {
     it('should return a string containing the message', () => {
       const message = 'Hello';
 
-      const result = myPackage(message);
+      const result = message;
 
       expect(result).toMatch(message);
     });
